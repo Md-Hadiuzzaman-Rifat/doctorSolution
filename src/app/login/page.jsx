c47ed59cc7f81c568232c8a18df7bc6867369a0e
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-09-05 13:03:51 +0600

    starting project

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from "../../components/Navigation/Navigatin"
+
+const page = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h2>This is Login page.</h2>
+        </div>
+    );
+};
+export default page;
